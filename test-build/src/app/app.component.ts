010c9c7aaa6f223b2293ffdcc9b9c57ad7fa16d1
unknown <10086676@olympus.com>
2020-01-27 16:54:12 +0900

    confirm build configuration

---
+++ test-build/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.styl']
+})
+export class AppComponent {
+  title = 'test-build';
+  env = environment.apiUrl;
+}
