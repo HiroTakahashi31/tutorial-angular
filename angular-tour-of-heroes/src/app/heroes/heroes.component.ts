836e121f80b3de8b66f88540beb9ed6a8f272bea
unknown <10086676@olympus.com>
2020-01-29 10:12:08 +0900

    No2 display list

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,18 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.styl']
 })
 export class HeroesComponent implements OnInit {
-  //hero = 'Windstorm';
-  hero:Hero={
-    id:1,
-    name:'Windstorm'
+  
+  heroes = HEROES;
+  selectedHero:Hero;
+
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
   }
+
+  // hero:Hero={
+  //   id:1,
+  //   name:'Windstorm'
+  // }
 
   constructor() { }
 
